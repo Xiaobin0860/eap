67c1ef69b9160e94eac2100754ff490f4bf37cb3
Li Xiaobin <l_xb@foxmail.com>
2022-05-04 22:54:49 +0800

    七周七 lua day3

---
+++ lua/七周七/day3/one_note_song.lua
@@ -0,0 +1,11 @@
+local NOTE_DOWN = 0x90
+local NOTE_UP = 0x80
+local NOTE_VELOCITY = 0x7f
+
+local function play(note)
+    midi_send(NOTE_DOWN, note, NOTE_VELOCITY)
+    while os.clock() < 1 do end
+    midi_send(NOTE_UP, note, NOTE_VELOCITY)
+end
+
+play(60)
---
+++ lua/七周七/day3/play.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "lua/lua.hpp"
+#include "rtmidi/RtMidi.h"
+
+namespace {
+RtMidiOut midi;
+
+int midi_send(lua_State* L)
+{
+    auto status = (unsigned char)lua_tonumber(L, -3);
+    auto data1 = (unsigned char)lua_tonumber(L, -2);
+    auto data2 = (unsigned char)lua_tonumber(L, -1);
+
+    std::vector<unsigned char> message{status, data1, data2};
+    midi.sendMessage(&message);
+    return 0;
+}
+}  // namespace
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <lua_file>" << std::endl;
+        return -1;
+    }
+
+    auto ports = midi.getPortCount();
+    if (ports < 1) {
+        std::cerr << "No MIDI ports available!" << std::endl;
+        return -1;
+    }
+    midi.openPort(0);
+
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+
+    lua_pushcfunction(L, midi_send);
+    lua_setglobal(L, "midi_send");
+
+    luaL_dofile(L, argv[1]);
+
+    lua_close(L);
+    return 0;
+}
