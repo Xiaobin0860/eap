d6bff4f0e7065faef6a9558c50dd8a5f6848a988
xl000 <l_xb@foxmail.com>
2022-10-04 23:30:47 +0800

    leetcode go: minAddToMakeValid

--- go/leetcode/leetcode202210_test.go
+++ go/leetcode/leetcode202210_test.go
@@ -15,6 +15,26 @@ func checkOnesSegment(s string) bool {
 	}
 }
 
+func minAddToMakeValid(s string) int {
+	left := 0
+	right := 0
+	for _, c := range s {
+		switch c {
+		case '(':
+			left += 1
+		case ')':
+			if left > 0 {
+				left -= 1
+			} else {
+				right += 1
+			}
+		default:
+			panic("invalid")
+		}
+	}
+	return left + right
+}
+
 func TestCheckOnesSegment(t *testing.T) {
 	assert.False(t, checkOnesSegment("1001"))
 	assert.True(t, checkOnesSegment("110"))
@@ -23,4 +43,13 @@ func TestCheckOnesSegment(t *testing.T) {
 	assert.True(t, checkOnesSegment("10"))
 	assert.False(t, checkOnesSegment("101"))
 	assert.False(t, checkOnesSegment("10101"))
+}
+
+func TestMinAddToMakeValid(t *testing.T) {
+	assert.Equal(t, 1, minAddToMakeValid("())"))
+	assert.Equal(t, 2, minAddToMakeValid("()()))"))
+	assert.Equal(t, 3, minAddToMakeValid("((("))
+	assert.Equal(t, 0, minAddToMakeValid("()(())"))
+	assert.Equal(t, 0, minAddToMakeValid(""))
+	assert.Equal(t, 3, minAddToMakeValid(")()))"))
 }
