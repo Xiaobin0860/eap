3b3adb0ffe661063f2042248b2223117c38edb19
xl000 <l_xb@foxmail.com>
2022-10-04 23:18:16 +0800

    leetcode rust: min_add_to_make_valid

--- rust/leetcode/tests/leetcode_test202210.rs
+++ rust/leetcode/tests/leetcode_test202210.rs
@@ -37,9 +37,62 @@ fn check_ones_segment(s: String) -> bool {
     true
 }
 
+/// # 使括号有效的最少添加
+///
+/// 只有满足下面几点之一，括号字符串才是有效的：
+///
+///    它是一个空字符串，或者
+///    它可以被写成 AB （A 与 B 连接）, 其中 A 和 B 都是有效字符串，或者
+///    它可以被写作 (A)，其中 A 是有效字符串。
+///
+/// 给定一个括号字符串 s ，移动N次，你就可以在字符串的任何位置插入一个括号。
+///
+///    例如，如果 s = "()))" ，你可以插入一个开始括号为 "(()))" 或结束括号为 "())))" 。
+///
+/// 返回 为使结果字符串 s 有效而必须添加的最少括号数。
+///
+/// ## 示例 1：
+///
+/// ```
+/// 输入：s = "())"
+/// 输出：1
+/// ```
+///
+/// ## 示例 2：
+///
+/// ```
+/// 输入：s = "((("
+/// 输出：3
+/// ```
+///
+/// ## 提示：
+///
+/// ```
+/// 1 <= s.length <= 1000
+/// s 只包含 '(' 和 ')' 字符。
+/// ```
+fn min_add_to_make_valid(s: String) -> i32 {
+    let mut left = 0;
+    let mut right = 0;
+    for c in s.as_str().chars() {
+        match c {
+            '(' => left += 1,
+            ')' => {
+                if left > 0 {
+                    left -= 1;
+                } else {
+                    right += 1;
+                }
+            }
+            _ => panic!("invalid"),
+        }
+    }
+    left + right
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::check_ones_segment;
+    use crate::*;
 
     #[test]
     fn test_check_ones_segment() {
@@ -50,5 +103,14 @@ mod tests {
         assert!(check_ones_segment("10".into()));
         assert!(!check_ones_segment("101".into()));
         assert!(!check_ones_segment("10101".into()));
+    }
+    #[test]
+    fn test_min_add_to_make_valid() {
+        assert_eq!(1, min_add_to_make_valid("())".into()));
+        assert_eq!(2, min_add_to_make_valid("()()))".into()));
+        assert_eq!(3, min_add_to_make_valid("(((".into()));
+        assert_eq!(0, min_add_to_make_valid("()(())".into()));
+        assert_eq!(0, min_add_to_make_valid("".into()));
+        assert_eq!(3, min_add_to_make_valid(")()))".into()));
     }
 }
