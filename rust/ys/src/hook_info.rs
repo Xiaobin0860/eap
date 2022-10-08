9ce2b604e48d729c5fee707f5a2e4d5028d36db9
xl000 <l_xb@foxmail.com>
2022-10-08 19:44:11 +0800

    search types and methods

--- rust/ys/src/hook_info.rs
+++ rust/ys/src/hook_info.rs
@@ -1,8 +1,85 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
+use tracing::{debug, trace};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 pub struct HookInfo {
     pub name: String,
     pub mp: String,
-    pub tp: String,
+    pub tp: Option<String>,
+    pub ename: Option<String>,
+    pub methods: Option<Vec<MethodInfo>>,
+}
+
+#[derive(Default)]
+pub struct TypeInfo {
+    pub name: String,
+    pub ename: String,
+    pub methods: Vec<String>,
+}
+
+impl TypeInfo {
+    pub fn new(name: String, ename: String) -> Self {
+        Self {
+            name,
+            ename,
+            methods: Vec::new(),
+        }
+    }
+}
+
+impl Display for TypeInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{{{}, {}, {}}}",
+            &self.name,
+            &self.ename,
+            self.methods.len()
+        )
+    }
+}
+
+impl HookInfo {
+    pub fn search_type(&mut self, re: &Regex, lines: &Vec<String>) -> Option<TypeInfo> {
+        for line in lines.iter() {
+            if re.is_match(line) {
+                trace!("{} match line {}", self.name, line);
+                //DO_APP_FUNC(0x030AC1E0, void, FFEKNPFBNFH__ctor_2, (FFEKNPFBNFH * __this,
+                let ss: Vec<_> = line.split(',').collect();
+                let ss: Vec<_> = ss[3].trim_start().split(' ').collect();
+                let ename = &ss[0][1..];
+                debug!("{} => {}", self.name, ename);
+                let mut info = TypeInfo::new(self.name.to_owned(), ename.to_owned());
+                self.ename = Some(ename.to_owned());
+                self.search_methods(lines, &mut info);
+                return Some(info);
+            }
+        }
+        None
+    }
+
+    fn search_methods(&self, lines: &Vec<String>, info: &mut TypeInfo) {
+        let re = &format!(
+            r"{}_(_[c]+tor.*|[a-zA-Z]+)(, \(|__Method|_\d+, \()",
+            info.ename
+        );
+        let re = Regex::new(re).unwrap();
+        for line in lines.iter() {
+            if re.is_match(line) {
+                info.methods.push(line.to_owned());
+            }
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct MethodInfo {
+    pub fname: String,
+    pub mp: Option<String>,
+    pub sig: Option<String>,
+    pub ename: Option<String>,
+    pub idx: usize,
+    pub offset: Option<u64>,
 }
--- rust/ys/src/main.rs
+++ rust/ys/src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use clap::Parser;
+use regex::Regex;
+use std::io::{self, Write};
 use std::path::Path;
+use std::{fs, io::BufRead};
 use tracing::{debug, info, trace};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use ys::HookInfo;
@@ -27,15 +30,50 @@ fn main() -> Result<()> {
         .init();
 
     let args = Args::parse();
-    info!("Finding {} offsets in {} ...", &args.gv, &args.gd);
+    let rys = &Path::new(&args.gd).join("rys");
+    let od = &rys.join(&args.gv);
+    info!("Finding {} offsets in {} od={od:?} ...", &args.gv, &args.gd);
 
-    let patterns_file = Path::new(&args.gd).join("patterns.json");
-    trace!("patterns_file={patterns_file:?}");
-    let patterns_string = std::fs::read_to_string(patterns_file)?;
-    let patterns: Vec<HookInfo> = serde_json::from_str(&patterns_string)?;
+    let pc_dir = &format!("PC{}", &args.gv);
+    let app_dir = &Path::new(&args.gd)
+        .join(pc_dir)
+        .join("inject")
+        .join("appdata");
+    let funcs_file = &app_dir.join("il2cpp-functions.h");
+    let types_file = &app_dir.join("il2cpp-types.h");
+    let patterns_file = &rys.join("patterns.json");
+    trace!("patterns_file={patterns_file:?}, funcs_file={funcs_file:?}, types_file={types_file:?}");
+    let patterns_string = fs::read_to_string(patterns_file)?;
+    let mut patterns: Vec<HookInfo> = serde_json::from_str(&patterns_string)?;
     debug!("patterns count: {}", patterns.len());
-    for pattern in patterns.iter() {
+
+    let lines: Vec<String> = io::BufReader::new(fs::File::open(funcs_file)?)
+        .lines()
+        .into_iter()
+        .map(|s| s.unwrap())
+        .collect();
+    debug!("func_lines size: {}", lines.len());
+    let mut app = io::BufWriter::new(fs::File::create(od.join("Application.h"))?);
+    for line in lines.iter() {
+        if line.contains(", Application_") {
+            app.write_all(line.as_bytes())?;
+            app.write_all("\n".as_bytes())?;
+        }
+    }
+    app.flush()?;
+    for pattern in patterns.iter_mut() {
         trace!("{pattern:?}");
+        let re = Regex::new(&pattern.mp)?;
+        let info = &pattern.search_type(&re, &lines).unwrap();
+        let out = &od.join(format!("{}.h", &info.name));
+        debug!("{info}, out={out:?}");
+        let mut w = io::BufWriter::new(fs::File::create(out)?);
+        for l in info.methods.iter() {
+            let l = l.replace(&info.ename, &info.name);
+            w.write_all(l.as_bytes())?;
+            w.write_all("\n".as_bytes())?;
+        }
+        w.flush()?;
     }
 
     Ok(())
